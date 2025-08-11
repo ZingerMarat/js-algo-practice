7074bcb332802563a526e644127ababa4cf54140
Zinger Marat <zingermarat@gmail.com>
2025-08-11 17:48:12 +0300

    Implement canExit function and add lastCommonMultiple with tests

--- Hard/canExit.js
+++ Hard/canExit.js
@@ -44,8 +44,45 @@ In a maze of size m x n, you enter at [0, 0] and exit at [m-1, n-1].
 There can be dead ends in a maze - one exit path is sufficient.
 */
 
-function canExit( /*args*/ ) {
-  //your code
+const canMove = (arr, level, start) => {
+  //can exit
+  if (level === arr.length - 1 && start === arr[0].length - 1 && arr[level][start] === 0) {
+    return true
+  }
+
+  //check bounderes
+  if (level < 0 || start < 0 || level >= arr.length || start >= arr[0].length) {
+    return false
+  }
+
+  //cant move
+  if (arr[level][start] !== 0) {
+    return false
+  }
+
+  arr[level][start] = "*"
+  console.table(arr)
+
+  if (canMove(arr, level, start + 1)) return true // right
+  if (canMove(arr, level + 1, start)) return true // down
+  if (canMove(arr, level, start - 1)) return true // left
+  if (canMove(arr, level - 1, start)) return true // up
+
+  return false
 }
 
-exports.solution = canExit;
+function canExit(arr) {
+  return canMove(arr, 0, 0)
+}
+
+exports.solution = canExit
+
+console.log(
+  canExit([
+    [0, 1, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [1, 1, 1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 0],
+  ])
+)
