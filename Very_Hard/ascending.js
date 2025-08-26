a6c500fc74fd1b36efcf08ac03aa9c273f81a8bd
Zinger Marat <zingermarat@gmail.com>
2025-08-26 14:05:19 +0300

    Remove canExit and leader functions along with their tests; refactor ascending function and tests for improved logic and clarity

--- Very_Hard/ascending.js
+++ Very_Hard/ascending.js
@@ -14,8 +14,56 @@ Notes
 A number can consist of any number of digits, so long as the numbers are adjacent to each other, and the string has at least two of them.
 */
 
-function ascending( /*args*/ ) {
+function ascending(string) {
   //your code
+
+  for (let i = Math.round(string.length / 2); i > 0; i--) {
+    let backetSize = i
+    const backets = []
+    let startIndex = 0
+    let endIndex = backetSize
+
+    while (startIndex < string.length) {
+      const number = string.slice(startIndex, endIndex)
+
+      backets.push(Number(number))
+
+      if (resizeNeeded(number, backetSize)) {
+        startIndex += backetSize
+        backetSize += 1
+        endIndex += backetSize
+      } else {
+        startIndex += backetSize
+        endIndex += backetSize
+      }
+    }
+
+    console.log(backets)
+
+    if (isConsecutive(backets)) {
+      return true
+    }
+  }
+
+  return false
 }
 
-exports.solution = ascending;
+const isConsecutive = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[i - 1] + 1) {
+      return false
+    }
+  }
+  return true
+}
+
+const resizeNeeded = (number, size) => {
+  if (String(Number(number) + 1).length > size) {
+    return true
+  }
+  return false
+}
+
+exports.solution = ascending
+
+console.log(ascending("91011"))
